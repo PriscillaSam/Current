d5372ceb8d502db48bdd5743ec74e61f786706d9
Iyke <iykebolt@gmail.com>
2017-07-01 13:16:41 +0100

    add exception handler for invalid input

--- NumberGuesser/NumberGuesser.cs
+++ NumberGuesser/NumberGuesser.cs
@@ -8,6 +8,8 @@ namespace NumberGuesser
 {
     class NumberGuesser
     {
+        static int userInput = 0;
+
         static void Main(string[] args)
         {
             GetAppInfo();
@@ -23,16 +25,24 @@ namespace NumberGuesser
 
                 Console.WriteLine("Guess a number between 1 and 10");
                 string input = Console.ReadLine();
-                int userInput = int.Parse(input);
+
+                //adding validation for exception
+                //int.TryParse(input, out userInput);
+
+               GetInput(input);
 
                 while (userInput != correctNumber)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Wrong Number!!! Please try again.... \a \n");
                     Console.ResetColor();
+                    
+                    //adding validation for handling exception
 
                     input = Console.ReadLine();
-                    userInput = int.Parse(input);
+
+                    //int.TryParse(input, out userInput);
+                    GetInput(input);
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -41,15 +51,14 @@ namespace NumberGuesser
 
                 Console.WriteLine("Would you like to play again? Yes or No");
                 string reply = Console.ReadLine().ToUpper();
-                if (reply == "YES")
+                if (reply == "YES" || reply == "Y")
                 {
                     continue;
                 }
-
-                //else
-                //{
-                //    return;
-                //}
+                else
+                {
+                    return;
+                }
 
             }//While loop ends here
 
@@ -76,5 +85,55 @@ namespace NumberGuesser
             Console.WriteLine("Sam-Iduh Priscilla");
             Console.ResetColor();
         }
+        //added new method
+        /*private static void GetINput(string userinput)
+        {
+            int input = 0;
+            try
+            {
+                input = int.Parse(userinput);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("nvalid format, type only numeric values");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Enter a value");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("maximum number exceeded");
+            }
+        }*/
+
+        private static void GetInput(string input)
+        {
+            while (true)
+            {
+                try
+                {
+                    userInput = int.Parse(input);
+                    return;
+                }
+                catch (FormatException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input: enter only numeric value");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("something bad happens");
+                    Console.ResetColor();
+                }
+                //retry
+                Console.WriteLine("Please, enter another value:");
+                input = Console.ReadLine();
+            }
+
+        }
+        
     }
 }
